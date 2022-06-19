3d3b1b37d25f676805364ecd73955e67f7fefd04
MScalopez <87540998+MScalopez@users.noreply.github.com>
2022-06-18 23:38:44 -0500

    Update init.sh

--- api-for-mongodb/01-create-mongodb-objects/node/init.sh
+++ api-for-mongodb/01-create-mongodb-objects/node/init.sh
@@ -1,6 +1,5 @@
 # Update node to Version 14.0.0, since the MongoDB driver requires ver 10+
-wget https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | tr -d '\r'
-sleep 2
+wget https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh && cat install.sh && echo "Am I safe????"
 sh ./install.sh
 source ~/.nvm/nvm.sh
 nvm install 14.0.0
