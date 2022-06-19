6722152de8bf5c1238005d63c9c2059d273a8fd1
MScalopez <87540998+MScalopez@users.noreply.github.com>
2022-06-18 21:20:59 -0500

    Update init.sh

---
+++ api-for-mongodb/01-create-mongodb-objects/node/init.sh
@@ -0,0 +1,26 @@
+# Update node to Version 14.0.0, since the MongoDB driver requires ver 10+
+wget https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh && cat install.sh && echo "Am I safe????"
+sh install.sh
+source ~/.nvm/nvm.sh
+nvm install 14.0.0
+npm install -g mongodb
+npm link mongodb
+
+
+# Create a MongoDB API account
+
+# Variable block
+ResourceGroup=$(az group list --query [].name --output tsv)
+ServerVersion="4.0"
+let "randomIdentifier=$RANDOM*$RANDOM"
+location=$(az group list --query [].location --output tsv)
+account="learn-account-cosmos-$randomIdentifier"
+
+# Create a Cosmos account for MongoDB API
+echo "Creating $account"
+az cosmosdb create --name $account --resource-group $ResourceGroup --kind MongoDB --server-version $ServerVersion --default-consistency-level Eventual --enable-automatic-failover true --locations regionName="$location"
+
+#Displaying the Connection String
+ConnectionString=$(az cosmosdb keys list --name $account --resource-group $ResourceGroup --type connection-strings --query connectionStrings[0].connectionString --output tsv)
+echo $ConnectionString
+
