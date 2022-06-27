6f940e75af12337aa49b9150e50114f7cb1b9085
MScalopez <87540998+MScalopez@users.noreply.github.com>
2022-06-26 21:43:40 -0500

    Add files via upload

--- api-for-mongodb/02-indexes-and-aggregation-pipelines/init.sh
+++ api-for-mongodb/02-indexes-and-aggregation-pipelines/init.sh
@@ -1,3 +1,14 @@
+# Download Git
+git clone https://github.com/MicrosoftLearning/mslearn-cosmosdb.git
+
+# Unzip collections
+cd ~/mslearn-cosmosdb/data
+unzip cosmic-works.zip
+mv cosmic-works-v1 database-v1
+mv cosmic-works-v2 database-v2
+mv cosmic-works-v3 database-v3
+mv cosmic-works-v4 database-v4
+cd ~/mslearn-cosmosdb/api-for-mongodb/02-indexes-and-aggregation-pipelines
 
 # Create a MongoDB API account
 
@@ -12,8 +23,16 @@ account="learn-account-cosmos-$randomIdentifier"
 echo "Creating $account"
 az cosmosdb create --name $account --resource-group $ResourceGroup --kind MongoDB --server-version $ServerVersion --default-consistency-level Eventual --enable-automatic-failover true --locations regionName="$location"
 
-#Displaying the Connection String
+# Get connection string
 ConnectionString=$(az cosmosdb keys list --name $account --resource-group $ResourceGroup --type connection-strings --query connectionStrings[0].connectionString --output tsv)
+
+# Install the MongoDB Python drivers
+python -m pip install pymongo
+
+# Load collection database-v1.customer
+python load-data.py -d database-v1 -c customer -cs $ConnectionString
+
+#Displaying the Connection String, Account Name and Resource Group Name
 echo "**************  Cosmos DB Account name ********************"
 echo $account
 echo "***********************************************************"
